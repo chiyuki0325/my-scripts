71be48232317a04f3d84a99a706a2514db58a77c
Yidaozhan Ya <yidaozhanyaqwq@gmail.com>
2023-02-17 20:41:59 +0800

    redmi

--- redmi_vc.py
+++ redmi_vc.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 import pulsectl
 import sys
-from math import ceil
+from math import ceil, floor
 import subprocess
+import time
 
 # Volume control utility for dickful Redmi Buds 3
 
@@ -13,7 +14,7 @@ def notify_send(right, left):
         [
             "notify-send",
             " 新音量:",
-            f"右耳:{right}%\n左耳:{left}%",
+            f"真实音量:{right}%\n(左耳:{left}%)",
             "-i",
             "pavucontrol",
             "-a",
@@ -21,6 +22,9 @@ def notify_send(right, left):
             "-e",
             "-t",
             "1000",
+            '-r',
+            # timestamp
+            str(int(int(time.time())/5))[2:],
         ],
     )
 
@@ -32,7 +36,7 @@ for card in pulsectl.Pulse("volume-control").card_list():
         card_id = card.name.split(".")[1]
         for sink in pulse.sink_list():
             if card_id in sink.name:
-                current_right_volume = ceil(ceil(sink.volume.values[1] * 100) / 5) * 5
+                current_right_volume = ceil(floor(sink.volume.values[1] * 100) / 5) * 5
                 match sys.argv[1]:
                     case "up":
                         new_right_volume = current_right_volume + 5
