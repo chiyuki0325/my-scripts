9ddd07b8148fc575b96736c1be80f29fd86ab429
Yidaozhan Ya <yidaozhanyaqwq@gmail.com>
2023-02-26 21:20:07 +0800

    tlyric

--- netease_lyric.py
+++ netease_lyric.py
@@ -6,6 +6,7 @@
 import requests
 import os
 import sys
+import re
 
 API_ROOT: str = "http://localhost:3000"
 
@@ -22,5 +23,22 @@ for root, _, files in os.walk(sys.argv[1]):
             url=f"{API_ROOT}/lyric?id={song_id}",
         )
         lyric: str = response.json()["lrc"]["lyric"]
+        if (
+            "tlyric" in response.json()
+            and response.json()["tlyric"]["lyric"] != ""
+            and (
+                re.compile("[\u3040-\u309f]+").search(lyric)
+                or re.compile("[\u30a0-\u30ff]+").search(lyric)
+            )
+        ):
+            print("tlyric")
+            lyric = response.json()["tlyric"]["lyric"]
+            for line in lyric.split("\n"):
+                if line == "":
+                    continue
+                text: str = ']'.join(line.split("]")[1:])
+                new_text: str = text.strip('(（)）')
+                if new_text != text:
+                    lyric = lyric.replace(text, new_text)
         with open(os.path.join(root, f"{song_name}.lrc"), "w") as f:
             f.write(lyric)
